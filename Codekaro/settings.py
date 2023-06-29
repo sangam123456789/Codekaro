fe96a56ccb769f970a0f85d664670804ea144a99
sangam123456789 <91962643+sangam123456789@users.noreply.github.com>
2023-06-29 16:51:10 +0530

    n

--- Codekaro/settings.py
+++ Codekaro/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os
 from pathlib import Path
-
+import warnings
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -138,3 +138,5 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+warnings.filterwarnings("ignore")
