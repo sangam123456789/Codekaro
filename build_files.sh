f382e17f0b5bfaab3f7af82ab49c90f7fb39a8af
sangam123456789 <91962643+sangam123456789@users.noreply.github.com>
2023-06-29 16:37:29 +0530

    new

--- build_files.sh
+++ build_files.sh
@@ -1,3 +1,4 @@
-pip install -r requirements.txt
-python3.9 manage.py collectstatic
-
+echo " BUILD START" 
+pip3 install -r requirements.txt 
+python3.9 manage.py collectstatic --noinput --clear 
+echo " BUILD END"
