0e2ffe14954fb4ce8fc99903aa2500a729edf15f
sangam123456789 <91962643+sangam123456789@users.noreply.github.com>
2023-08-04 22:49:58 +0530

    hoja

--- Codekaro/settings.py
+++ Codekaro/settings.py
@@ -24,7 +24,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'gaurav.singh911805@gmail.com'
+EMAIL_HOST_USER = 'codesniper112233@gmail.com'
 EMAIL_HOST_PASSWORD = 'izbhjyvwtmggizwx'
 
 
