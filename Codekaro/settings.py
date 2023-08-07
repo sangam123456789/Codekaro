9a426fc659ea4a4ddf78bca298f71bce7e7cca9d
sangam123456789 <91962643+sangam123456789@users.noreply.github.com>
2023-08-07 19:07:35 +0530

    First commit

--- Codekaro/settings.py
+++ Codekaro/settings.py
@@ -15,8 +15,8 @@ import warnings
 import dj_database_url
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-file_path = os.path.join(BASE_DIR, 'dbbackup', 'loggin_config.yaml')
+# BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+# file_path = os.path.join(BASE_DIR, 'dbbackup', 'loggin_config.yaml')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
