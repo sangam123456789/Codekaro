79f9f7a197f286aed5b4378e75bea943efa2fc02
sangam123456789 <91962643+sangam123456789@users.noreply.github.com>
2023-08-08 11:39:50 +0530

    First commit

--- Codekaro/settings.py
+++ Codekaro/settings.py
@@ -47,7 +47,6 @@ INSTALLED_APPS = [
     'user_agents',
     'django.contrib.admin',
     'django.contrib',
-    '_sqlite3',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
