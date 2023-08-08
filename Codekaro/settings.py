9ffd186636c2a3844a35cfbfa4c41df10e05e8b1
sangam123456789 <91962643+sangam123456789@users.noreply.github.com>
2023-08-08 13:11:16 +0530

    First commit

--- Codekaro/settings.py
+++ Codekaro/settings.py
@@ -13,6 +13,8 @@ import os
 from pathlib import Path
 import warnings
 import dj_database_url
+import pymysql
+pymysql.install_as_MySQLdb()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 # BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
