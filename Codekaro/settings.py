da02cf97389c9452d582218fccc9634dca685d4e
sangam123456789 <91962643+sangam123456789@users.noreply.github.com>
2023-08-08 11:21:52 +0530

    First commit

--- Codekaro/settings.py
+++ Codekaro/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 import os
 from pathlib import Path
 import warnings
-import sqlite3
+import sqlite3 , _sqlite3
 import dj_database_url
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
