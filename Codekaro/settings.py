7496b1acdd09342817041041e13119edfca40843
sangam123456789 <91962643+sangam123456789@users.noreply.github.com>
2023-08-08 20:34:01 +0530

    First commit

--- Codekaro/settings.py
+++ Codekaro/settings.py
@@ -95,10 +95,10 @@ WSGI_APPLICATION = 'Codekaro.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
 
     # 'default': {
     #     'ENGINE': 'django.db.backends.postgresql',
